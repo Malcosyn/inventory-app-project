1dcd3c23b18641750257feb98e1b578db9aa6f8d
Malcosyn <makmal.learn@gmail.com>
2026-03-31 21:20:06 +0700

    Add sign up feature

---
+++ lib/secrets/supabase_secret.dart
@@ -0,0 +1,5 @@
+
+class SupabaseSecret {
+  static const supabaseUrl = 'https://fbjniglixlnmfmttkixp.supabase.co';
+  static const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZiam5pZ2xpeGxubWZtdHRraXhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NjA1NDQsImV4cCI6MjA4OTEzNjU0NH0.rGo6id5DN-T8gBOJH9HEq-6Vr4AIDFeEVPsm2y3Ayc0';
+}
